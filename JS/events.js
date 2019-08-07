1cce54dd2f594f937d3e6563199358fe3142a140
Pushpinder Kaur <kaurrevature@outlook.com>
2019-08-08 02:21:19 +0530

    More DOM Manipulation and events

--- JS/events.js
+++ JS/events.js
@@ -21,7 +21,7 @@ function show(){// event handler
 
 // DOM  Manipulation: It's used to manipulate html and css for a document using JS DomApi
 //1. To get reference we have methods from document object.
-/*document.queryselector('mention selector as #id, .class, tag')
+/*document.queryselector('mention selector as #id, .class, tag')- mordern way of selecting elements by is id, class name, tagsname using the CSS selectors
   document.getElementById("id")
   document.getElementByTagsName("p")
   document.getElementByClassName("foo")
@@ -31,7 +31,7 @@ var btn1 = document.querySelector("#btn1");// this will give you reference of HT
 //btn.onclick=show;
 btn1.addEventListener('click',show);// another way to register an event
 
-var btn2=document.getElementById("btn2");
+var btn2=document.getElementById("btn2");// this fetches the element by id only
 btn2.onclick=()=>{
     var name=prompt('Enter your full name');
     document.write("Welcome "+name);// write function is used to write in the body of the web page
@@ -47,4 +47,27 @@ function addCarsToList(){
     li.textContent=carName;
     var list=document.querySelector("#lstCars");
     list.appendChild(li);
+}
+// This checks the validation of the form
+var btnsubmit=document.querySelector("#submit");
+btnsubmit.addEventListener('click', validateForm);
+function validateForm(){
+    var name=document.querySelector("#name").value;
+    var email=document.querySelector("#email").value;
+    var query=document.querySelector("#query").value;
+    if(name==""||name==null)
+        alert('Name field cannot be blank');
+    if(email==""||email==null)
+        alert('Email field cannot be blank');
+    if(query==""||query==null)
+        alert('Query field cannot be blank');    
+}
+
+//Changes the name field value to uppercase
+var nameField=document.querySelector("#name");
+nameField.onkeyup=()=>{
+    nameField.value=nameField.value.toUpperCase();
+    nameField.style.background="yellow";
+    nameField.style.color="blue";
+    //alert(nameValue.toUpperCase());
 }
